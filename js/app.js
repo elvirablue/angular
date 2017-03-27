779ed7e04296edd97079d1b12b1d04b93d78a274
student <student>
2017-03-27 21:42:57 +0700

    222222

--- js/app.js
+++ js/app.js
@@ -6,11 +6,25 @@
 	var app = angular.module ('store',[]);
 
 	app.controller("StoreController", function(){
-		this.product = prod;
+		this.products = prods;
 	});
-	var prod = {
-		name: "Учет сейфов",
-		price: 4.99,
-		description: "Учитываем сейфы"
-	}	
+	var prods = [{
+			name: "1-Учет сейфов",
+			price: 4.99,
+			description: "Учитываем сейфы",		
+			canPurchase: false
+		},
+		{
+			name: "2-Учет сейфов",
+			price: 24.99,
+			description: "Учитываем сейфы",		
+			canPurchase: true
+
+		},
+		{
+			name: "3-Учет сейфов",
+			price: 34.99,
+			description: "Учитываем сейфы",		
+			canPurchase: false
+		}]
 })();
